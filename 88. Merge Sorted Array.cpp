bd142e5181e2576446ea2f1a3ca2cfc852d61901
Kiruthya Ramesh <kiruthyasp31@gmail.com>
2020-07-30 01:10:01 +0530

    88. Merge Sorted Array(pointer approach)

--- 88. Merge Sorted Array.cpp
+++ 88. Merge Sorted Array.cpp
@@ -12,6 +12,8 @@ You may assume that nums1 has enough space (size that is greater or equal to m +
 
 */
 
+/*
+////////////using sort function
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
@@ -20,6 +22,49 @@ public:
         nums1.insert(nums1.end(),nums2.begin(),nums2.end());
         sort(nums1.begin(),nums1.end());
         nums2.clear();
+        
+    }
+};
+*/
+
+
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int j;
+  /*    1 2 3
+            j
+         2 5 6
+             i
+    */
+    if(m!=0 && n!=0)
+    {
+        for(int i=n-1;i>=0;i--)
+        {
+            int last = nums1[m-1];
+            for(j= m-2 ; j>=0 && nums1[j]>nums2[i] ; j--) //find the smallest integer in arr1 that is greater than the ith element of arr2
+                nums1[j+1]=nums1[j];
+            
+            if(j!=m-2 || last > nums2[i])
+            {
+                nums1[j+1]=nums2[i];
+                nums2[i]=last;
+            }
+        }
+    }
+      /*  
+        for(int i=0;i<m;i++)
+            cout<<nums1[i]<<" ";
+        for(int i=0;i<n;i++)
+            cout<<nums2[i]<<" ";
+        */
+          j= m;
+          for(int i=0;i<n;i++)
+          {
+              nums1[j++]=nums2[i];
+              
+          }
         
     }
 };
