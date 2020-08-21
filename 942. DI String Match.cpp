a300e8c009d546eaa0d2584c77ec976581fec6d9
Kiruthya Ramesh <2017103546@annauniv.edu.in>
2020-08-21 13:36:59 +0530

    942. DI String Match

---
+++ 942. DI String Match.cpp
@@ -0,0 +1,56 @@
+//Phoenix_RK
+
+
+/*
+
+https://leetcode.com/problems/di-string-match/
+
+Given a string S that only contains "I" (increase) or "D" (decrease), let N = S.length.
+
+Return any permutation A of [0, 1, ..., N] such that for all i = 0, ..., N-1:
+
+If S[i] == "I", then A[i] < A[i+1]
+If S[i] == "D", then A[i] > A[i+1]
+ 
+
+Example 1:
+
+Input: "IDID"
+Output: [0,4,1,3,2]
+Example 2:
+
+Input: "III"
+Output: [0,1,2,3]
+Example 3:
+
+Input: "DDI"
+Output: [3,2,0,1]
+ 
+
+Note:
+
+1 <= S.length <= 10000
+S only contains characters "I" or "D".
+*/
+
+class Solution {
+public:
+    vector<int> diStringMatch(string S) {
+        
+        int min=0,max=S.length();
+        vector<int> res;
+        
+        for(int i=0;i<S.length();i++)
+        {
+            if(S[i]=='I')
+                res.push_back(min++);
+            if(S[i]=='D')
+                res.push_back(max--);
+        }
+        if(S[S.length()-1]=='I')
+           res.push_back(min++);
+        else
+           res.push_back(max--);
+        return res;
+    }
+};
