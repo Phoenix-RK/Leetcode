8f1551c83a028e8dedb9323c5e3726bb1a2dc7c5
Kiruthya Ramesh <2017103546@annauniv.edu.in>
2020-11-22 16:57:43 +0530

    74. Search a 2D Matrix

--- 74. Search a 2D Matrix.cpp
+++ 74. Search a 2D Matrix.cpp
@@ -59,3 +59,38 @@ public:
         return false;
     }
 };
+
+//Binary search
+
+ bool searchMatrix(vector<vector<int>>& matrix, int target) {
+      
+        if(matrix.size()==0)
+            return false;
+        
+        int r=matrix.size();
+        int c=matrix[0].size();
+        int left=0,right=r*c-1;
+        
+        while(left<=right)
+        {
+             int mid=left+(right-left)/2;
+            
+             int mid_ele=matrix[mid/c][mid%c];
+            
+            if(mid_ele==target)
+            {
+                return true;
+            }
+            else if(mid_ele<target)
+            {
+                left=mid+1;
+            }
+            else if(mid_ele>target)
+            {
+                right=mid-1;
+            }
+        
+        }
+       
+        return false;
+    }
