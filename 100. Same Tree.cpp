06c559fadeffbbe45e70c08af3b0d3f5873984b1
Kiruthya Ramesh <2017103546@annauniv.edu.in>
2020-12-06 16:13:08 +0530

    100. Same Tree

---
+++ 100. Same Tree.cpp
@@ -0,0 +1,66 @@
+//Phoenix_RK
+
+/*
+
+https://leetcode.com/problems/same-tree/
+
+Given two binary trees, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+
+Example 1:
+
+Input:     1         1
+          / \       / \
+         2   3     2   3
+
+        [1,2,3],   [1,2,3]
+
+Output: true
+Example 2:
+
+Input:     1         1
+          /           \
+         2             2
+
+        [1,2],     [1,null,2]
+
+Output: false
+Example 3:
+
+Input:     1         1
+          / \       / \
+         2   1     1   2
+
+        [1,2,1],   [1,1,2]
+
+Output: false
+
+
+
+
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+     
+        if(p==NULL && q==NULL)
+            return true;
+        if(p==NULL || q==NULL)
+            return false;
+        return (p->val==q->val)&& isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+    }
+};
